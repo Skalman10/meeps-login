525b8bd040c7453dd29fd3a6811bd1dee6700252
skalman10 <isak.thyrel@elev.ga.ntig.se>
2022-05-09 10:18:04 +0200

    meeps post route (för att skapa nya meeps)

--- routes/api.js
+++ routes/api.js
@@ -2,25 +2,32 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-router.get('/meeps', async function (req, res, next) {
+router.post('/meeps', async (req, res, next) => {
+    const meep = req.body.body;
+
     await pool.promise()
-        .query(`SELECT * FROM iskthl_meeps`)
-        .then(([rows, fields]) => {
-            console.log(rows);
-            res.json({
-                meeps: {
-                    data: rows
-                }
-            });
+        .query('INSERT INTO iskthl_meeps (body) VALUES (?)', [meep])
+        .then((response) => {
+            console.log(response[0].affectedRows);
+            if (response[0].affectedRows === 1) {
+                res.redirect('/');
+            } else {
+                res.status(400).json({
+                    meeps: {
+                        error: 'Invalid meep'
+                    }
+                });
+            }
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                tasks: {
-                    error: `Error getting meeps`
+                meeps: {
+                    error: 'Error posting meep'
                 }
             })
         });
-});
+}
+);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const pool = require('../database');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   await pool.promise()
-    .query(`SELECT * FROM iskthl_meeps ORDER BY created_at`)
+    .query(`SELECT * FROM iskthl_meeps ORDER BY created_at DESC`)
     .then(([rows, fields]) => {
       console.log(rows);
       res.render('index.njk', {
