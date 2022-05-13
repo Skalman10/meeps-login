05fb25c93111fd3762025e9ebf305b3c5da42235
skalman10 <isak.thyrel@elev.ga.ntig.se>
2022-05-13 09:20:28 +0200

    login route som sparar användaren i cookies

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 var bcrypt = require('bcrypt');
 const pool = require('../database');
+var session = require('express-session');
 
 router.post('/meeps', async (req, res, next) => {
     const meep = req.body.body;
@@ -37,14 +38,38 @@ router.post('/register', async function (req, res, next) {
 
     bcrypt.hash(password, 10, async function (err, hash) {
         await pool.promise()
-        .query('INSERT INTO iskthl_login (username, password) VALUES (?,?)', [username,hash])
-        .then(([rows, fields]) => {
-            req.session.user = username;
-            res.redirect("/");
-        }).catch(err => {
-            console.log(err)
-        });
+            .query('INSERT INTO iskthl_login (username, password) VALUES (?,?)', [username, hash])
+            .then(([rows, fields]) => {
+                req.session.user = username;
+                res.redirect("/");
+            }).catch(err => {
+                console.log(err)
+            });
     });
 })
 
+router.post('/login', async function (req, res, next) {
+    const username = req.body.user;
+    const password = req.body.password;
+    await pool.promise()
+        .query('SELECT * FROM iskthl_login WHERE username = ?', [username])
+        .then(([rows, fields]) => {
+            const user = rows[0];
+            if (user) {
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if (result) {
+                        req.session.user = username;
+                        console.log(req.session);
+                        return res.redirect("/");
+                    } else {
+                        res.status(500);
+                    }
+
+                });
+            } else throw "error";
+        })
+        .catch(err => {
+            res.status(500);
+        });
+});
 module.exports = router;
